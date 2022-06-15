7920dd4380188485f998f590a90669c4cb42e2a2
RusuMax1m <100152342+RusuMax1m@users.noreply.github.com>
2022-06-15 15:33:37 +0300

    Add files via upload

---
+++ js/call.js
@@ -0,0 +1,38 @@
+﻿var call_form = document.getElementById("call-form");
+//call_form.tel.focus();
+
+function file_get_contents( url ) { 
+    var req = null;
+
+    try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {
+        try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {
+            try { req = new XMLHttpRequest(); } catch(e) {}
+        }
+    }
+    if (req == null) throw new Error('XMLHttpRequest not supported');
+    req.open("GET", url, false);
+    req.send(null);
+    return req.responseText;
+}
+
+
+call_form.addEventListener ('submit', function(event) {
+	event.preventDefault();
+		if (this.tel.value) {
+          file_get_contents("http://sms.ru/sms/send?api_id=8768b717-c25e-27b4-8527-f760b3784243&to=79968025361&text=Телефон " + this.tel.value + " Имя "+ this.name.value + "&partner_id=52108");
+		} else {
+			alert("Введите номер телефона");
+		}
+
+});
+
+function afterRequest() {
+	var respText = this.responseText;
+	var obj = JSON.parse(respText);
+	if (obj.result) {
+		alert("Заявка принята!\nСпасибо!");
+		$('.offer').arcticmodal('close'); 
+	} else {
+		alert("Ошибка!\nПозвоните нам по телефону +7(987)522-41-00");
+	}
+};
---
+++ js/i_orders.js
@@ -0,0 +1,43 @@
+﻿var form = document.getElementById("order_form");
+var lt = '';
+var ln = '';
+// navigator.geolocation.getCurrentPosition(success, error);
+
+function file_get_contents( url ) { 
+    var req = null;
+
+    try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {
+        try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {
+            try { req = new XMLHttpRequest(); } catch(e) {}
+        }
+    }
+    if (req == null) throw new Error('XMLHttpRequest not supported');
+    req.open("GET", url, false);
+    req.send(null);
+    return req.responseText;
+}
+
+form.onsubmit = function() {
+
+	
+    file_get_contents("http://sms.ru/sms/send?api_id=8768b717-c25e-27b4-8527-f760b3784243&to=79968025361&text=Сломался: " + this.elements.type.value + " Модель "+ this.elements.model.value + " Дефект " + this.elements.defect.value + " Имя " + this.elements.client.value + " Номер телефона " + this.elements.phone_number.value +"&partner_id=52108");
+
+   // xhr.open("get", "http://sms.ru/sms/send?api_id=8768b717-c25e-27b4-8527-f760b3784243&to=79968025361&text=Сломался: " + this.elements.type.value + " Модель "+ this.elements.model.value + " Дефект " + this.elements.defect.value + " Имя " + this.elements.client.value + " Номер телефона " + this.elements.phone_number.value +"&partner_id=52108");
+
+	//xhr.send();
+	////xhr.send("api_id=8768b717-c25e-27b4-8527-f760b3784243&to=79968025361&text=Сломался: " + this.elements.type.value + " Модель "+ this.elements.model.value + " Дефект " + this.elements.defect.value + " Имя " + this.elements.client.value + " Номер телефона " + this.elements.phone_number.value+	"&partner_id=52108");
+
+}
+
+function answer() {
+	var respText = this.responseText;
+	var obj = JSON.parse(respText);
+	if (obj.result) {
+		alert("Спасибо, ваша заявка принята!\nМы свяжемся с вами в ближайшее время");
+		/*document.location.href = "/";*/
+		$('#myModal').modal('hide');
+	} else {
+		alert("Ошибка, оставьте заявку по телефону: +7(987)522-41-00");
+	}
+}
+
